8c27a6471ec6354b5bb227491833b2d2dfdd960e
Yousef Khalil <yousefkhalil599@gmail.com>
2023-04-16 23:10:02 +0300

    656

--- src/RandomName/NewJFrame.java
+++ src/RandomName/NewJFrame.java
@@ -37,7 +37,7 @@ public class NewJFrame extends javax.swing.JFrame {
         setBackground(new java.awt.Color(255, 255, 255));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBackground(new java.awt.Color(0, 153, 102));
+        jPanel1.setBackground(new java.awt.Color(153, 153, 153));
 
         jTextField1.setText("");
         jTextField1.addActionListener(new java.awt.event.ActionListener() {
