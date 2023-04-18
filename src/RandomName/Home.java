3cc1688418002b696e215d3ad7e1fb1d441e99cc
RawadAltagiuri <rawadaltagury@gmail.com>
2023-04-18 15:28:05 +0300

    Made the menu change colors when selected

--- src/RandomName/Home.java
+++ src/RandomName/Home.java
@@ -32,9 +32,9 @@ public class Home extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         CheckInTabButton = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
+        RoomsTabButton = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
-        jPanel3 = new javax.swing.JPanel();
+        EventsTabButton = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         CheckInTabPanel = new javax.swing.JPanel();
@@ -80,10 +80,10 @@ public class Home extends javax.swing.JFrame {
                 .addGap(22, 22, 22))
         );
 
-        jPanel2.setBackground(new java.awt.Color(0, 153, 153));
-        jPanel2.addMouseListener(new java.awt.event.MouseAdapter() {
+        RoomsTabButton.setBackground(new java.awt.Color(0, 153, 153));
+        RoomsTabButton.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jPanel2MouseClicked(evt);
+                RoomsTabButtonMouseClicked(evt);
             }
         });
 
@@ -91,27 +91,27 @@ public class Home extends javax.swing.JFrame {
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Rooms");
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        javax.swing.GroupLayout RoomsTabButtonLayout = new javax.swing.GroupLayout(RoomsTabButton);
+        RoomsTabButton.setLayout(RoomsTabButtonLayout);
+        RoomsTabButtonLayout.setHorizontalGroup(
+            RoomsTabButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(RoomsTabButtonLayout.createSequentialGroup()
                 .addGap(48, 48, 48)
                 .addComponent(jLabel2)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        RoomsTabButtonLayout.setVerticalGroup(
+            RoomsTabButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(RoomsTabButtonLayout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(20, Short.MAX_VALUE))
         );
 
-        jPanel3.setBackground(new java.awt.Color(0, 153, 153));
-        jPanel3.addMouseListener(new java.awt.event.MouseAdapter() {
+        EventsTabButton.setBackground(new java.awt.Color(0, 153, 153));
+        EventsTabButton.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jPanel3MouseClicked(evt);
+                EventsTabButtonMouseClicked(evt);
             }
         });
 
@@ -119,18 +119,18 @@ public class Home extends javax.swing.JFrame {
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Events");
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
+        javax.swing.GroupLayout EventsTabButtonLayout = new javax.swing.GroupLayout(EventsTabButton);
+        EventsTabButton.setLayout(EventsTabButtonLayout);
+        EventsTabButtonLayout.setHorizontalGroup(
+            EventsTabButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(EventsTabButtonLayout.createSequentialGroup()
                 .addGap(48, 48, 48)
                 .addComponent(jLabel3)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
+        EventsTabButtonLayout.setVerticalGroup(
+            EventsTabButtonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(EventsTabButtonLayout.createSequentialGroup()
                 .addGap(24, 24, 24)
                 .addComponent(jLabel3)
                 .addContainerGap(24, Short.MAX_VALUE))
@@ -141,17 +141,17 @@ public class Home extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(CheckInTabButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(RoomsTabButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(EventsTabButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(CheckInTabButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(RoomsTabButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(EventsTabButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 433, Short.MAX_VALUE))
         );
 
@@ -164,11 +164,9 @@ public class Home extends javax.swing.JFrame {
         jTabbedPane1.setAutoscrolls(true);
         jTabbedPane1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         jTabbedPane1.setMaximumSize(new java.awt.Dimension(3300, 32767));
-        jTabbedPane1.addHierarchyBoundsListener(new java.awt.event.HierarchyBoundsListener() {
-            public void ancestorMoved(java.awt.event.HierarchyEvent evt) {
-            }
-            public void ancestorResized(java.awt.event.HierarchyEvent evt) {
-                jTabbedPane1AncestorResized(evt);
+        jTabbedPane1.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jTabbedPane1StateChanged(evt);
             }
         });
 
@@ -215,19 +213,55 @@ public class Home extends javax.swing.JFrame {
 
     private void CheckInTabButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_CheckInTabButtonMouseClicked
         jTabbedPane1.setSelectedIndex(0);
+        
     }//GEN-LAST:event_CheckInTabButtonMouseClicked
 
-    private void jTabbedPane1AncestorResized(java.awt.event.HierarchyEvent evt) {//GEN-FIRST:event_jTabbedPane1AncestorResized
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTabbedPane1AncestorResized
-
-    private void jPanel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel2MouseClicked
+    private void RoomsTabButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_RoomsTabButtonMouseClicked
         jTabbedPane1.setSelectedIndex(1);
-    }//GEN-LAST:event_jPanel2MouseClicked
+    }//GEN-LAST:event_RoomsTabButtonMouseClicked
 
-    private void jPanel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel3MouseClicked
+    private void EventsTabButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_EventsTabButtonMouseClicked
         jTabbedPane1.setSelectedIndex(2);
-    }//GEN-LAST:event_jPanel3MouseClicked
+    }//GEN-LAST:event_EventsTabButtonMouseClicked
+        
+    private void jTabbedPane1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jTabbedPane1StateChanged
+        
+        
+        int selectedIndex = jTabbedPane1.getSelectedIndex();
+        Color Unselected = new Color(0, 153, 153);
+        Color Selected = new Color(0,204,204);
+        // Check if the currently selected tab is the tab corresponding to the JPanel
+        if (selectedIndex == 0) {
+            // If so, set the background color of the JPanel to a different color
+            CheckInTabButton.setBackground(Selected);
+        } else {
+            // If not, revert the background color of the JPanel to the original color
+            CheckInTabButton.setBackground(Unselected);
+        }
+        
+        if (selectedIndex == 1) {
+            // If so, set the background color of the JPanel to a different color
+            RoomsTabButton.setBackground(Selected);
+        } else {
+            // If not, revert the background color of the JPanel to the original color
+            RoomsTabButton.setBackground(Unselected);
+        }
+        
+        if (selectedIndex == 1) {
+            // If so, set the background color of the JPanel to a different color
+            RoomsTabButton.setBackground(Selected);
+        } else {
+            // If not, revert the background color of the JPanel to the original color
+            RoomsTabButton.setBackground(Unselected);
+        }
+        if (selectedIndex == 2) {
+            // If so, set the background color of the JPanel to a different color
+            EventsTabButton.setBackground(Selected);
+        } else {
+            // If not, revert the background color of the JPanel to the original color
+            EventsTabButton.setBackground(Unselected);
+        }
+    }//GEN-LAST:event_jTabbedPane1StateChanged
 
     /**
      * @param args the command line arguments
@@ -267,13 +301,13 @@ public class Home extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel CheckInTabButton;
     private javax.swing.JPanel CheckInTabPanel;
+    private javax.swing.JPanel EventsTabButton;
+    private javax.swing.JPanel RoomsTabButton;
     private javax.swing.JPanel Tab;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JTabbedPane jTabbedPane1;
     // End of variables declaration//GEN-END:variables
